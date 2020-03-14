9ed4c64b45d5e4a657c1dff89b8fd966ec507503
David Prandzioch <hello+git@davd.eu>
2020-03-14 08:36:10 +0100

    Smaller adjustments, german translation (#954)

--- dev/Promises/User/Populator.js
+++ dev/Promises/User/Populator.js
@@ -93,7 +93,7 @@ class PromisesUserPopulator extends AbstractBasicPromises {
 						);
 					} else {
 						// If there are already subfolders cached but none are provided, remove existing cache entries
-						oCacheFolder.subFolders(this.folderResponseParseRec(sNamespace, [], expandedFolders));
+						oCacheFolder.subFolders([]);
 					}
 
 					aList.push(oCacheFolder);
